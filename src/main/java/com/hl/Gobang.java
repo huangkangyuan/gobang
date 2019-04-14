fc07e5b9a4c75237a3015e8aa8da102e2a9816d9
root <614772276@qq.com>
2019-04-14 10:07:23 +0800

    Gobang_JFrame

--- src/main/java/com/hl/Gobang.java
+++ src/main/java/com/hl/Gobang.java
@@ -7,6 +7,20 @@ public class Gobang extends JFrame {
     public Gobang(){
         this.setSize(500, 500);
         this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setTitle("五子棋");
+
+        //最大化  可选
+        this.setResizable(false);
+
+        JMenuBar jMenuBar = new JMenuBar();
+        JMenu jMenu = new JMenu("选项");
+
+        //菜单栏
+        jMenuBar.add(jMenu);
+
+        this.setJMenuBar(jMenuBar);
+
     }
 
     public static void main(String[] args) {
