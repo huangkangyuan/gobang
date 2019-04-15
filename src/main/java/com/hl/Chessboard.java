e53ce0c86d816fb85027d3db0b8467b8737d8793
root <614772276@qq.com>
2019-04-15 23:15:15 +0800

    draw_image_in_chessBoard

--- src/main/java/com/hl/Chessboard.java
+++ src/main/java/com/hl/Chessboard.java
@@ -1,4 +1,15 @@
 package com.hl;
 
-public class Chessboard {
+import javax.swing.*;
+import java.awt.*;
+
+public class Chessboard extends JPanel {
+
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        Image image = new ImageIcon("images/qipan1.png").getImage();
+        graphics.drawImage(image, 0, 0,this.getWidth(),this.getHeight(), this);
+    }
 }
